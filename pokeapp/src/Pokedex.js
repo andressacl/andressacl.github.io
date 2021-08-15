72c684aed52ead52fedae75ee14e95b21c666f04
Andressa Colaço <andressacolaco@usp.br>
2021-08-14 23:28:06 -0300

    testando

---
+++ pokeapp/src/Pokedex.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import'./App.css';
+import logo from './logo.svg';
+import axios from 'axios';
+import { Grid } from '@material-ui/core';
+
+const Pokedex = (props) => {
+    //const classes = useStyles();
+    const { history } = props;
+    const [pokemonData, setPokemonData] = useState({});
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+        .then(function (response) {
+            const { data } = response;
+            const { results } = data;
+            const newPokemonData = {};
+            results.forEach((pokemon, index) => {
+            newPokemonData[index + 1] = {
+                id: index + 1,
+                name: pokemon.name,
+                sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                index + 1
+                }.png`,
+            };
+            });
+            setPokemonData(newPokemonData);
+        });
+    }, []);
+
+    const handleSearchChange = (e) => {
+        setFilter(e.target.value);
+      };
+
+    const toFirstCharUppercase = (name) => {
+         return name.charAt(0).toUpperCase() + name.slice(1)
+      };
+
+    const getPokemonCard = (pokemonId) => {
+        const { id, name, sprite } = pokemonData[pokemonId];
+    return (
+        <div className="container">
+            <div className='card' onClick={() => history.push(`/${id}`)}>
+                <div className="button-id">{`${id}`}</div>
+                <img src={sprite}></img>
+                <div className='card-name'>{`${toFirstCharUppercase(name)}`}</div>
+                <div className='card-types'></div>
+            </div>
+        </div>
+    )};
+
+
+    return (
+        <div className="App">
+
+        <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <form > 
+            <label>
+                <input
+                type="text"
+                onChange={handleSearchChange}
+                placeholder="Pesquise por nome"
+                />
+            </label>
+            </form>
+
+            <div id="button-container">
+    
+            <a href="https://youtu.be/dQw4w9WgXcQ">
+            <button id="surpreenda-me" className="button">Surpreenda-me!</button>
+            </a>
+            </div>
+        </header>
+
+        <body className='card-grid'>
+            <Grid container spacing={2}>
+            {Object.keys(pokemonData).map(
+                (pokemonId) =>
+                pokemonData[pokemonId].name.includes(filter) &&
+                getPokemonCard(pokemonId)
+            )}
+            </Grid>
+        </body>
+
+        </div>
+        );
+};
+
+export default Pokedex;
---
+++ pokeapp/src/Pokemon.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import './index.css';
+import { CircularProgress } from "@material-ui/core";
+import { toFirstCharUppercase } from "./constants";
+import axios from "axios";
+import { Link } from 'react-router-dom';
+import './App.css';
+
+const Pokemon = (props) => {
+  const { match } = props;
+  const { params } = match;
+  const { pokemonId } = params;
+  const [pokemon, setPokemon] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+      .then(function (response) {
+        const { data } = response;
+        setPokemon(data);
+      })
+      .catch(function (error) {
+        setPokemon(false);
+      });
+  }, [pokemonId]);
+
+  const generatePokemonJSX = (pokemon) => {
+    const { name, id, species, height, weight, types, sprites, abilities, stats} = pokemon;
+    console.log(pokemon);
+    const fullImageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`;
+    const { front_default } = sprites;
+    return (
+      <div className='maiorDeTodos'>
+        <div className= "primeira-tela">
+              <div className= "info-container">    
+                  <div className= "namenum">
+                      <span id = "name">{toFirstCharUppercase(name)}</span>
+                      <span id = "num">{` Nº ${id}`}</span>
+                  </div>
+
+                  <div className= "definition">
+                        <div><p></p></div>
+                        {`Species: ${species.name}`}
+                  </div>
+
+                  <div className= "type">
+                        <p>Type</p>
+                        <div className = "each-type">
+                              {types.map((typeInfo, idx) => {
+                              const { type } = typeInfo;
+                              const { name } = type;
+                              return <div id= "type" key={idx}> <p>{name}</p> </div>
+                              })}
+                        </div>
+                  </div>
+              </div>
+
+              <div className = "pokeimg"><img src={front_default} alt="pokemon"></img></div>
+              </div>
+            
+            <div className='segunda-tela'>
+            <div className ="quadro">
+                <div className ="caracteristica">
+                    {`Height: ${height} ft`}
+                </div>
+                <div className ="caracteristica">
+                  {`Weight: ${weight} lbs`}
+                </div>
+              
+                <div className ="abilities">
+                  <p id="abilities-title">Abilities:</p>
+                  {abilities.map((abilityInfo, idx) => {
+                              const { ability } = abilityInfo;
+                              const { name } = ability;
+                              return <div id= "abilities-container" key={idx}><p id='ability'>{name}</p></div>
+                              })}
+                
+                </div>
+                </div>
+
+              <div className='stats'>
+              {stats.map((statsInfo, idx) => {
+                              const { stat } = statsInfo;
+                              const { base_stat } = statsInfo;
+                              const { name } = stat;
+                              return (
+                              <>
+                              <div id= "stat" key={idx}><p>{name}: {base_stat}</p></div>
+                              </>
+                              );
+                              })}
+              </div>
+            </div>
+            
+            {/* <div className='evolution-container'>
+              <div className='evolution'><img src={`http://pokeapi.co/media/sprites/pokemon/${id}.png`}></img></div>
+              <div className='evolution' src={`http://pokeapi.co/media/sprites/pokemon/${id+1}.png`}></div>
+              <div className='evolution'></div>
+            </div>
+            */}
+
+      </div>
+    )
+};
+
+return (
+    <>
+      {pokemon === undefined && <CircularProgress />}
+      {pokemon !== undefined && pokemon && generatePokemonJSX(pokemon)}
+      {pokemon === false && <p> Pokemon not found</p>}
+
+      {pokemon !== undefined && (
+        <div className='button' id='voltar'>
+        <Link to="/">Voltar</Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Pokemon;
